5c0f358dfaf1968f4aa0febe34620aacefc76504
Kenneth <kennethyoung.2960@gmail.com>
2024-03-16 04:08:24 -0500

    012 corrected and finished

--- CW012-ExesAndOhs/main.js
+++ CW012-ExesAndOhs/main.js
@@ -1,3 +1,18 @@
 function XO(str) {
+    // Need to set toLowerCase() to avoid any comparative issues
+    str = str.toLowerCase();
+
+    let xCount = 0;
+    let oCount = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'x') {
+            xCount++;
+        }
+        else if (str[i] === 'o') {
+            oCount++;
+        }
+    }
+    return xCount === oCount;
     //code here
 }
