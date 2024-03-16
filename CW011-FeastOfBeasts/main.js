1f9a2d165206893a0c5b2d321d00665f5e0cf424
Kenneth <kennethyoung.2960@gmail.com>
2024-03-16 04:00:31 -0500

    011 Finished

--- CW011-FeastOfBeasts/main.js
+++ CW011-FeastOfBeasts/main.js
@@ -1,3 +1,5 @@
 function feast(beast, dish) {
-    
+    const firstLetter = beast[0];
+    const lastLetter = beast[beast.length - 1];
+    return dish.startsWith(firstLetter) && dish.endsWith(lastLetter);
 }
