ce81cd2840840568cb529623f22a7afd0cb9b280
Kenneth <kennethyoung.2960@gmail.com>
2024-03-16 01:49:15 -0500

    Even or Odd function finished

---
+++ CW003-EvenOrOdd/main.js
@@ -0,0 +1,5 @@
+function evenOrOdd(number) {
+    return (number % 2 === 0) ? "Even" : "Odd"
+}
+// In this we use % to determing if the remainder of number / 2 is = 0
+// If this equals true, we return Even and if false we return Odd.
