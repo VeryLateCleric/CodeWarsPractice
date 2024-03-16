b315f46c2ff609b6d24dc3e9ebd95f50f328c8d5
Kenneth <kennethyoung.2960@gmail.com>
2024-03-16 02:38:01 -0500

    Added div by zero edge case to 006

--- CW006-BasicMathematicalOperations/main.js
+++ CW006-BasicMathematicalOperations/main.js
@@ -7,9 +7,15 @@ function basicOp(operation, value1, value2){
         case '*':
             return value1 * value2;
         case '/':
-            return value1 / value2;
+            if (value2 !== 0) {
+                return value1 / value2;
+            } else {
+                return "Division by zero is not allowed";
+            }
+        default:
+            return "Invalid operator";
     }
-  }
+}
 
 // This test does not have any values equal to zero.
 // However if we want to provide a case for this we can using an if else
