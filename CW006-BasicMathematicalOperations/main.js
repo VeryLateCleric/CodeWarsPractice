cdcfaed52455b820c712b1654b90ca3dbe00f4d2
Kenneth <kennethyoung.2960@gmail.com>
2024-03-16 03:22:13 -0500

    Playing Banjo function working perfectly

--- CW006-BasicMathematicalOperations/main.js
+++ CW006-BasicMathematicalOperations/main.js
@@ -19,3 +19,15 @@ function basicOp(operation, value1, value2){
 
 // This test does not have any values equal to zero.
 // However if we want to provide a case for this we can using an if else
+
+// Following is a more clearly written version of the same as above:
+
+function basicOperation(operator, value1, value2) {
+    switch (operator) {
+        case '+': return value1 + value2;
+        case '-': return value1 - value2;
+        case '*': return value1 * value2;
+        case '/': return value1 / value2;
+        default: return 'Operation must be one of + - * /';
+    }
+}
