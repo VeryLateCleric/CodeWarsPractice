1c9c2cc3fed1927133635871af57bdb2c60d1241
Kenneth <kennethyoung.2960@gmail.com>
2024-03-16 02:36:09 -0500

    GradeBook Finished. Want add edge case for div by zero

--- CW006-BasicMathematicalOperations/main.js
+++ CW006-BasicMathematicalOperations/main.js
@@ -1,4 +1,15 @@
 function basicOp(operation, value1, value2){
-
-    return 0;
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+    }
   }
+
+// This test does not have any values equal to zero.
+// However if we want to provide a case for this we can using an if else
