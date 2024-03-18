0b1853badb28eea23abb97a92c1447fbae441f10
Kenneth <kennethyoung.2960@gmail.com>
2024-03-18 00:34:25 -0500

    016 Vowel Count solved

---
+++ CW016-VowelCount/main.js
@@ -0,0 +1,10 @@
+function getCount(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (let char of str) {
+      if (vowels.includes(char)) {
+        count++
+      }
+    }
+    return count;
+  }
