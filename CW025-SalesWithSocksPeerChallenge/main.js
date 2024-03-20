b7ec14f39451d3971c603964bac0fd84420ee637
Kenneth <kennethyoung.2960@gmail.com>
2024-03-19 20:12:06 -0500

    Passing selfmade tests, trying solution

--- CW025-SalesWithSocksPeerChallenge/main.js
+++ CW025-SalesWithSocksPeerChallenge/main.js
@@ -1,17 +1,17 @@
-const socks = [1, 2, 1, 2, 1, 3, 2, 3, 2, 4, 4, 5, 5, 1, 2, 1, 6, 3]; //n=18
-const socks2 = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4];//n=20
+const socks = [1, 2, 1, 2, 1, 3, 2, 3, 2, 4, 4, 5, 5, 1, 2, 1, 6, 3]; //n=18 pairs: 7
+const socks2 = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4];//n=20 pairs: 9
 
 function sockMerchant(n, ar) {
-    const colorCounts = {}
-    console.log(colorCounts);
+    const colorCounts = {};
     ar.forEach(color => colorCounts[color] = (colorCounts[color] || 0) + 1);
-    console.log(colorCounts);
+    console.log("ColorCounts:", colorCounts);
+    console.log("SockCount:", ar.length);
 
     let sockPairs = 0;
     for (const color in colorCounts) {
-        const pairs = (2+2)// some Math Placeholder
+        const pairs = Math.floor(colorCounts[color] / 2)
         sockPairs += pairs
-        console.log(sockPairs);
+        console.log("We got another pair of socks! Socks:", sockPairs);
     }
 
     return sockPairs;
