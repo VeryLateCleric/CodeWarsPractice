774db3ab392284e9e15670436810bc57a2f3cf70
Kenneth <kennethyoung.2960@gmail.com>
2024-03-19 20:02:36 -0500

    basic tests written

--- CW025-SalesWithSocksPeerChallenge/main.js
+++ CW025-SalesWithSocksPeerChallenge/main.js
@@ -1,5 +1,21 @@
-function sockMerchant(n, ar) {
-    const colorCounts = []
+const socks = [1, 2, 1, 2, 1, 3, 2, 3, 2, 4, 4, 5, 5, 1, 2, 1, 6, 3]; //n=18
+const socks2 = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4];//n=20
 
-    return colorCounts;
+function sockMerchant(n, ar) {
+    const colorCounts = {}
+    console.log(colorCounts);
+    ar.forEach(color => colorCounts[color] = (colorCounts[color] || 0) + 1);
+    console.log(colorCounts);
+
+    let sockPairs = 0;
+    for (const color in colorCounts) {
+        const pairs = (2+2)// some Math Placeholder
+        sockPairs += pairs
+        console.log(sockPairs);
+    }
+
+    return sockPairs;
 }
+
+console.log(sockMerchant(18, socks));
+console.log(sockMerchant(20, socks2));
